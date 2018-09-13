6bad585633966c3c774be4916ec2049b43867650
hellolovesocial <taylor.minako375@gmail.com>
2018-09-14 07:39:13 +0900

    fixed debug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,17 @@
 module ApplicationHelper
     def get_twitter_card_info(user)
-        p "================"
-        p user
-        p "================"
         twitter_card = {}
         if user
-            p "================"
-            p "きてるよ"
-            p "================"
-        
             twitter_card[:url] = "https://summer-third-seminer-ayanooooon.c9users.io/users/#{user.id}/share"
             twitter_card[:title] = "#{user.name}がたくさん食べたよ"
             twitter_card[:description] = "#{user.name}さんの、おいしいものを食べた気分になれる画像を見てみましょう"
             twitter_card[:image] = "https://summer-third-seminer-ayanooooon.c9users.io/finals/final#{user.id}.jpg"
-            p "================"
-            p   twitter_card[:url]
-            p   twitter_card[:title]
-            p   twitter_card[:description]
-            p   twitter_card[:image]
-            p "================"
+            p "=======twitter_card========="
+            p  twitter_card[:url]
+            p  twitter_card[:title]
+            p  twitter_card[:description]
+            p  twitter_card[:image]
+            p "============================"
         else
             twitter_card[:url] = 'https://summer-third-seminer-ayanooooon.c9users.io/'
             twitter_card[:title] ='かおピタ｜顔をあてはめてご飯の気分'
